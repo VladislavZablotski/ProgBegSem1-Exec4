8ee9ffa73f106dadef7c9cb1e946e3f037706509
unknown <zab_vl@tutby>
2023-03-12 01:10:15 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+System.Console.WriteLine("Введите число 1:");
+int number1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int number2 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3:");
+int number3 =Convert.ToInt32(Console.ReadLine());
+
+int Max = number1;
+
+if (number2>=Max)
+{
+    Max = number2;
+}
+if (number3>=Max)
+{
+    Max = number3;
+}
+Console.Write("Максимальное число равно ");
+Console.WriteLine(Max);
